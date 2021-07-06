5b8d13cde6531d5595fa34c0e67fe7c520146873
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-07 01:19:46 +0530

    update and delete category

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -27,30 +27,30 @@ const categoryCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
-//   deleteCategory: async (req, res) => {
-//     try {
-//       const products = await Products.findOne({ category: req.params.id });
-//       if (products)
-//         return res.status(400).json({
-//           msg: "Please delete all products with a relationship.",
-//         });
+    deleteCategory: async (req, res) => {
+      try {
+        // const products = await Products.findOne({ category: req.params.id });
+        // if (products)
+        //   return res.status(400).json({
+        //     msg: "Please delete all products with a relationship.",
+        //   });
 
-//       await Category.findByIdAndDelete(req.params.id);
-//       res.json({ msg: "Deleted a Category" });
-//     } catch (err) {
-//       return res.status(500).json({ msg: err.message });
-//     }
-//   },
-//   updateCategory: async (req, res) => {
-//     try {
-//       const { name } = req.body;
-//       await Category.findOneAndUpdate({ _id: req.params.id }, { name });
+        await Category.findByIdAndDelete(req.params.id);
+        res.json({ msg: "Deleted a Category" });
+      } catch (err) {
+        return res.status(500).json({ msg: err.message });
+      }
+    },
+    updateCategory: async (req, res) => {
+      try {
+        const { name } = req.body;
+        await Category.findOneAndUpdate({ _id: req.params.id }, { name });
 
-//       res.json({ msg: "Updated a category" });
-//     } catch (err) {
-//       return res.status(500).json({ msg: err.message });
-//     }
-//   },
+        res.json({ msg: "Updated a category" });
+      } catch (err) {
+        return res.status(500).json({ msg: err.message });
+      }
+    },
 };
 
 module.exports = categoryCtrl;
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -6,11 +6,11 @@ const authAdmin = require("../middleware/authAdmin");
 router
   .route("/category")
   .get(categoryCtrl.getCategories)
-//   .post(auth, authAdmin, categoryCtrl.createCategory);
+  .post(auth, authAdmin, categoryCtrl.createCategory);
 
-// router
-//   .route("/category/:id")
-//   .delete(auth, authAdmin, categoryCtrl.deleteCategory)
-//   .put(auth, authAdmin, categoryCtrl.updateCategory);
+router
+  .route("/category/:id")
+  .delete(auth, authAdmin, categoryCtrl.deleteCategory)
+  .put(auth, authAdmin, categoryCtrl.updateCategory);
 
 module.exports = router;
