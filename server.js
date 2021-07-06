3c75082dabd35b7bded6f79d008e7b1c5fbf88bb
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-07 02:18:45 +0530

    upload and delete images on cloudinary

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(
 // Routes
 app.use("/user", require("./routes/userRouter"));
 app.use("/api", require("./routes/categoryRouter"));
+app.use("/api", require("./routes/upload"));
 
 // Connect to mongodb
 const URI = process.env.MONGODB_URL;
