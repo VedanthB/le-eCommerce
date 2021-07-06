458e627b209c0535c8491451a8119204f209a320
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-07 01:10:23 +0530

    category model/ ctrl / router
    getCategories
    createCategory

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(
 
 // Routes
 app.use("/user", require("./routes/userRouter"));
+app.use("/api", require("./routes/categoryRouter"));
 
 // Connect to mongodb
 const URI = process.env.MONGODB_URL;
