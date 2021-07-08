3e4285a163536122188a802b090ee3983509988c
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-09 04:05:59 +0530

    admin and user product page

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,24 +1,64 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { GlobalState } from "../../../GlobalState";
 import Loading from "../utils/loading/Loading";
 import ProductItem from "../utils/productItem/ProductItem";
 
 function Products() {
-  const state = useContext(GlobalState)
-  const [products] = state.productsAPI.products
+  const state = useContext(GlobalState);
+  const [products, setProducts] = state.productsAPI.products;
+  const [isAdmin] = state.userAPI.isAdmin;
+  const [token] = state.token;
+  // const [callback, setCallback] = state.productsAPI.callback;
+  const [loading, setLoading] = useState(false);
+  const [isCheck, setIsCheck] = useState(false);
 
+  const handleCheck = (id) => {
+    products.forEach((product) => {
+      if (product._id === id) product.checked = !product.checked;
+    });
+    setProducts([...products]);
+  };
 
+  const checkAll = () => {
+    products.forEach((product) => {
+      product.checked = !isCheck;
+    });
+    setProducts([...products]);
+    setIsCheck(!isCheck);
+  };
+
+  if (loading)
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
   return (
     <>
-    <div className="products">
-      {
-        products.map(product => {
-          return <ProductItem key={product._id} product={product}/>
-        })
-      }
+      {isAdmin && (
+        <div className="delete-all">
+          <span>Select all</span>
+          <input type="checkbox" checked={isCheck} onChange={checkAll} />
+          {/* <button onClick={deleteAll}>Delete ALL</button> */}
+        </div>
+      )}
+
+      <div className="products">
+        {products.map((product) => {
+          return (
+            <ProductItem
+              key={product._id}
+              product={product}
+              isAdmin={isAdmin}
+              // deleteProduct={deleteProduct}
+              handleCheck={handleCheck}
+            />
+          );
+        })}
       </div>
+
       {products.length === 0 && <Loading />}
-      </>
+    </>
   );
 }
 
--- client/src/components/mainpages/utils/productItem/ProductItem.js
+++ client/src/components/mainpages/utils/productItem/ProductItem.js
@@ -1,29 +1,26 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-// import BtnRender from './BtnRender'
+import BtnRender from './BtnRender'
 
-function ProductItem({product}) {
+function ProductItem({product, isAdmin, deleteProduct, handleCheck}) {
 
     return (
-      <div className="product_card">
-        <img src={product.images.url} alt="" />
+        <div className="product_card">
+            {
+                isAdmin && <input type="checkbox" checked={product.checked}
+                onChange={() => handleCheck(product._id)} />
+            }
+            <img src={product.images.url} alt="" />
 
-        <div className="product_box">
-          <h2 title={product.title}>{product.title}</h2>
-          <span>${product.price}</span>
-          <p>{product.description}</p>
-        </div>
+            <div className="product_box">
+                <h2 title={product.title}>{product.title}</h2>
+                <span>${product.price}</span>
+                <p>{product.description}</p>
+            </div>
 
-        <div className="row_btn">
-          <Link id="btn_buy" to="#!">
-            Buy
-          </Link>
-          <Link id="btn_view" to={`detail/${product._id}`}>
-            View
-          </Link>
+            
+            <BtnRender product={product} deleteProduct={deleteProduct} />
         </div>
-      </div>
-    );
+    )
 }
 
 export default ProductItem;
