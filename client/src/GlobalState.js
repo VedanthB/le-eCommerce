a360f912f33c8f76c9409bfcb61bbfdb7e54bfbc
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-08 23:46:01 +0530

    Login / Register + css + refresh token  globalState

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from "react";
+import axios from "axios";
+import React, { createContext, useEffect, useState } from "react";
 import ProductsAPI from "./api/ProductsAPI";
 
 export const GlobalState = createContext();
@@ -6,14 +7,26 @@ export const GlobalState = createContext();
 export const DataProvider = ({ children }) => {
   const [token, setToken] = useState(false);
 
+  useEffect(() => {
+    const firstLogin = localStorage.getItem("firstLogin");
+    if (firstLogin) {
+      const refreshToken = async () => {
+        const res = await axios.get("/user/refresh_token");
+
+        setToken(res.data.accesstoken);
+
+        setTimeout(() => {
+          refreshToken();
+        }, 10 * 60 * 1000);
+      };
+      refreshToken();
+    }
+  }, []);
+
   const state = {
     token: [token, setToken],
     productsAPI: ProductsAPI(),
-  }
+  };
 
-  return (
-    <GlobalState.Provider value={state}>
-      {children}
-    </GlobalState.Provider>
-  )
+  return <GlobalState.Provider value={state}>{children}</GlobalState.Provider>;
 };
