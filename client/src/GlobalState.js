e6e483ca32604d40af52d2ffc0b9cb17caf3b24c
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-09 03:59:04 +0530

    admin and user header

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { createContext, useEffect, useState } from "react";
 import ProductsAPI from "./api/ProductsAPI";
+import UserAPI from "./api/UserAPI";
 
 export const GlobalState = createContext();
 
@@ -26,6 +27,7 @@ export const DataProvider = ({ children }) => {
   const state = {
     token: [token, setToken],
     productsAPI: ProductsAPI(),
+    userAPI: UserAPI(token),
   };
 
   return <GlobalState.Provider value={state}>{children}</GlobalState.Provider>;
