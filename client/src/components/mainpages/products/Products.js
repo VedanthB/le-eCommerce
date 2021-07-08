0fb608026a62053b21a6e2d4f5f787b59a105054
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-08 23:24:38 +0530

    Loading Loader

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalState } from "../../../GlobalState";
+import Loading from "../utils/loading/Loading";
 import ProductItem from "../utils/productItem/ProductItem";
 
 function Products() {
@@ -8,13 +9,16 @@ function Products() {
 
 
   return (
+    <>
     <div className="products">
       {
         products.map(product => {
           return <ProductItem key={product._id} product={product}/>
         })
       }
-    </div>
+      </div>
+      {products.length === 0 && <Loading />}
+      </>
   );
 }
 
