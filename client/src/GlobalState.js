27f418e55c019892e7e52a6cf091e00b19521583
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-14 01:57:18 +0530

    Categories page/ create category client

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,7 +1,9 @@
-import axios from "axios";
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import ProductsAPI from "./api/ProductsAPI";
 import UserAPI from "./api/UserAPI";
+import CategoriesAPI from "./api/CategoriesAPI";
+
+import axios from "axios";
 
 export const GlobalState = createContext();
 
@@ -28,6 +30,7 @@ export const DataProvider = ({ children }) => {
     token: [token, setToken],
     productsAPI: ProductsAPI(),
     userAPI: UserAPI(token),
+    categoriesAPI: CategoriesAPI(),
   };
 
   return <GlobalState.Provider value={state}>{children}</GlobalState.Provider>;
