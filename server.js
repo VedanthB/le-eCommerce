d878ab6f6e8c5cb4467545abc784da91b9db8c0b
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-14 03:50:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -37,6 +38,12 @@ mongoose.connect(
   }
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
